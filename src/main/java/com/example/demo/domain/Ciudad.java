d9090722d1f43cf2073fdfe9544226cada559bd7
Wilmer Sumba <pilimandi54@gmail.com>
2022-07-07 21:26:53 -0500

    creacion de ciudades y provincias

---
+++ src/main/java/com/example/demo/domain/Ciudad.java
@@ -0,0 +1,33 @@
+package com.example.demo.domain;
+
+import javax.persistence.Column;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Ciudad {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(updatable = false, nullable = false)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nombre;
+	
+	@ManyToOne
+	@JoinColumn(name ="Id_provincia")
+	private Provincia provincia;
+
+	
+}
